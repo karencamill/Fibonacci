986970a823588e1c8dbeea1d11625b598b1f8fd3
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-03-16 19:07:48 -0300

    Update FibonacciInterativo.java

--- FibonacciInterativo/src/fibo/FibonacciInterativo.java
+++ FibonacciInterativo/src/fibo/FibonacciInterativo.java
@@ -8,14 +8,14 @@ public class FibonacciInterativo {
 		// TODO Auto-generated method stub
 		
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Digite o número de termos de sequências: ");
+		System.out.println("Digite o número de termos da sequência: ");
 		int termos = teclado.nextInt();
 		
 		int anterior = 0;
 		int atual = 1;
 		
 			 System.out.println("Sequência de Fibonacci:");
-		        for (int i = 0; i < termos; i++) {
+		           for (int i = 0; i < termos; i++) {
 		            System.out.print(anterior + " ");
 		            int proximo = anterior + atual;
 		            anterior = atual;
